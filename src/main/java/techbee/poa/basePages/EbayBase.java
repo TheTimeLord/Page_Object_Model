24a65c69ffd1574878113dbe17cabed5e746a6b8
TheTimeLord <brianandrus2@gmail.com>
2021-04-19 16:01:31 -0700

    Created first iteration of EbayBase methods, reorganized some inheritance features.

--- src/main/java/techbee/poa/basePages/EbayBase.java
+++ src/main/java/techbee/poa/basePages/EbayBase.java
@@ -1,5 +1,75 @@
 package techbee.poa.basePages;
 
-public class EbayBase {
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
+public class EbayBase {
+	
+	private WebDriver driver;
+	
+	
+	// Constructor
+	public EbayBase(WebDriver d) {
+		driver = d;
+	}
+	
+	// Header Methods
+	
+	public void signin() {
+		WebElement ele = driver.findElement(By.linkText("Sign in"));
+		ele.click();
+	}
+	
+	public void dailyDeals() {
+		WebElement ele = driver.findElement(By.linkText("Daily Deals"));
+		ele.click();
+	}
+	
+	public void brandOutlet() {
+		WebElement ele = driver.findElement(By.linkText("Brand Outlet"));
+		ele.click();
+	}
+	
+	public void helpContact() {
+		WebElement ele = driver.findElement(By.linkText("Help & Contact"));
+		ele.click();
+	}
+	
+	public void sell() {
+		WebElement ele = driver.findElement(By.linkText("Sell"));
+		ele.click();
+	}
+	
+	public void Watchlist() {
+		WebElement ele = driver.findElement(By.linkText("Watchlist"));
+		ele.click();
+	}
+	
+	public void myEbay() {
+		WebElement ele = driver.findElement(By.linkText("My eBay"));
+		ele.click();
+	}
+	
+	public void notifications() {
+		WebElement ele = driver.findElement(By.id("gh-Alerts-i"));
+		ele.click();
+	}
+	
+	public void clickShoppingCart() {
+		WebElement ele = driver.findElement(By.className("gh-cart-icon"));
+		ele.click();
+	}
+	
+	// Extra methods
+	
+	public void scrollDown() {
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.scrollBy(0,document.body.scrollHeight)");
+	}
+	
+	public void connectHome() {
+		driver.get("https://www.ebay.com");
+	}
 }
--- src/main/java/techbee/poa/main/project.java
+++ src/main/java/techbee/poa/main/project.java
@@ -1,5 +1,27 @@
 package techbee.poa.main;
 
-public class project {
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import techbee.poa.basePages.*;
+
+public class project {
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "/dev/Webdrivers/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		EbayBase ebay = new EbayBase(driver);
+		
+		ebay.connectHome();
+		
+		ebay.signin();
+		Thread.sleep(1200);
+		ebay.connectHome();
+		
+		ebay.dailyDeals();
+		Thread.sleep(1200);
+		ebay.connectHome();
+		
+	}
 }

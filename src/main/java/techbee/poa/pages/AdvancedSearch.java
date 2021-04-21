5fb301891a1bbcdf3b68e6b810c7ca9595b5ab42
TheTimeLord <brianandrus2@gmail.com>
2021-04-20 20:51:54 -0700

    Added methods for sidebar in AdvancedSearch

--- src/main/java/techbee/poa/pages/AdvancedSearch.java
+++ src/main/java/techbee/poa/pages/AdvancedSearch.java
@@ -1,6 +1,8 @@
 package techbee.poa.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class AdvancedSearch extends EbayBase {
 	
@@ -9,19 +11,33 @@ public class AdvancedSearch extends EbayBase {
 	}
 	
 	public void clickFindItems() {
-		
+		WebElement ele = driver.findElement(By.linkText("Find items"));
+		ele.click();
 	}
 	
 	public void clickOnEbayMotors() {
-		
+		WebElement ele = driver.findElement(By.linkText("On eBay Motors"));
+		ele.click();
 	}
 	
 	public void clickBySeller() {
-		
+		WebElement ele = driver.findElement(By.linkText("By seller"));
+		ele.click();
 	}
 	
 	public void clickByItemNumber() {
-		
+		WebElement ele = driver.findElement(By.linkText("By item number"));
+		ele.click();
+	}
+	
+	public void clickItemsInStores() {
+		WebElement ele = driver.findElement(By.linkText("Items in stores"));
+		ele.click();
+	}
+	
+	public void clickFindStores() {
+		WebElement ele = driver.findElement(By.linkText("Find Stores"));
+		ele.click();
 	}
 	
 }

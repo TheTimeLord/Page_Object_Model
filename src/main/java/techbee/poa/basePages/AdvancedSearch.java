ccca02c1f0bcaffd9662ea7b6b1b677dda7577e6
TheTimeLord <brianandrus2@gmail.com>
2021-04-20 20:45:09 -0700

    Added AdvancedSearch class

---
+++ src/main/java/techbee/poa/basePages/AdvancedSearch.java
@@ -0,0 +1,27 @@
+package techbee.poa.basePages;
+
+import org.openqa.selenium.WebDriver;
+
+public class AdvancedSearch extends EbayBase {
+	
+	public AdvancedSearch(WebDriver d) {
+		super(d);
+	}
+	
+	public void clickFindItems() {
+		
+	}
+	
+	public void clickOnEbayMotors() {
+		
+	}
+	
+	public void clickBySeller() {
+		
+	}
+	
+	public void clickByItemNumber() {
+		
+	}
+	
+}
